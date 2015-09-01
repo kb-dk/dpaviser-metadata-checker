392211198bf023f501b29863ee6ec21e1bd7a9ca
Thorbjoern Ravn Andersen <tra@statsbiblioteket.dk>
2015-09-01 18:09:41 +0200

    Initial XPath extraction of PDF-status.

--- dpaviser-metadata-checker-checkers/src/main/java/dk/statsbiblioteket/dpaviser/metadatachecker/pdf/JHoveInvoker.java
+++ dpaviser-metadata-checker-checkers/src/main/java/dk/statsbiblioteket/dpaviser/metadatachecker/pdf/JHoveInvoker.java
@@ -1,6 +1,8 @@
 package dk.statsbiblioteket.dpaviser.metadatachecker.pdf;
 
 
+import com.google.common.io.ByteStreams;
+import org.apache.ws.commons.util.NamespaceContextImpl;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -11,33 +13,66 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class JHoveInvoker {
 
 
-
     public static void main(String[] args) throws Exception {
-        ProcessBuilder pb = new ProcessBuilder(Arrays.<String>asList("jhove", "-h", "xml", "-m", "pdf-hul",
-                "/home/tra/Skrivebord/pdf-sample.pdf"));
-        Process process = pb.start();
+        List<String> command = Arrays.<String>asList("jhove", "-h", "xml", "-m", "pdf-hul",
+                "/home/tra/Skrivebord/pdf-sample.pdf");
 
-        InputStream inputStream = process.getInputStream();
+        byte[] outputBytes = invokeCommand(command);
+
+        // inputstream copied to memory
+
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(inputStream);
-
-        process.waitFor();
+        Document document = builder.parse(new ByteArrayInputStream(outputBytes));
 
         // ---
 
         printDocument(document, System.out);
 
+        // Stolen from IteratorForFedora3.java
+        XPathFactory xpathFactory = XPathFactory.newInstance();
+        XPath xPath = xpathFactory.newXPath();
+
+        NamespaceContextImpl context = new NamespaceContextImpl();
+        context.startPrefixMapping("jhove", "http://hul.harvard.edu/ois/xml/ns/jhove");
+        xPath.setNamespaceContext(context);
+
+        XPathExpression statusXpath = xPath.compile("/jhove:jhove/jhove:repInfo/jhove:status/text()");
+
+        String status = statusXpath.evaluate(document);
+        System.out.println(status);
+
+
     }
+
+    public static byte[] invokeCommand(List<String> command) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        Process process = processBuilder.start();
+
+        InputStream inputStream = process.getInputStream();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ByteStreams.copy(inputStream, baos);
+        process.waitFor(60, TimeUnit.SECONDS);
+        return baos.toByteArray();
+    }
+
     public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
         // http://stackoverflow.com/a/2325407/53897
         TransformerFactory tf = TransformerFactory.newInstance();
