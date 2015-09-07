6b645b5167e0c4e80a823acac922044f8aee679a
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-09-07 15:56:20 +0200

    Reverted to newspaper way of handling schema checks. Added dumb schematron check

--- dpaviser-metadata-checker-component/src/main/java/dk/statsbiblioteket/dpaviser/metadatachecker/MetadataEventHandlerSupplier.java
+++ dpaviser-metadata-checker-component/src/main/java/dk/statsbiblioteket/dpaviser/metadatachecker/MetadataEventHandlerSupplier.java
@@ -1,13 +1,17 @@
 package dk.statsbiblioteket.dpaviser.metadatachecker;
 
 import dk.statsbiblioteket.dpaviser.metadatachecker.helper.JHoveCommandPipe;
-import dk.statsbiblioteket.dpaviser.metadatachecker.infomedia.NewsMLEventHandler;
 import dk.statsbiblioteket.dpaviser.metadatachecker.pdf.PDFEventHandler;
 import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.TreeEventHandler;
+import dk.statsbiblioteket.newspaper.metadatachecker.AttributeSpec;
+import dk.statsbiblioteket.newspaper.metadatachecker.SchemaValidatorEventHandler;
+import dk.statsbiblioteket.newspaper.metadatachecker.SchematronValidatorEventHandler;
 import dk.statsbiblioteket.newspaper.metadatachecker.caches.DocumentCache;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.function.Supplier;
 
@@ -21,12 +25,15 @@ public class MetadataEventHandlerSupplier implements Supplier<List<TreeEventHand
     final private DocumentCache documentCache;
     private final ResultCollector resultCollector;
     private Properties properties;
+    private Map<String, AttributeSpec> attributeConfigs;
 
 
     public MetadataEventHandlerSupplier(Properties properties, DocumentCache documentCache, ResultCollector resultCollector) {
         this.properties = properties;
         this.documentCache = documentCache;
         this.resultCollector = resultCollector;
+        attributeConfigs  = new HashMap<>();
+        attributeConfigs.put(".xml",new AttributeSpec(".xml","NewsML_1.2-infomedia.xsd","NewsML_1.2-infomedia.sch","NewsML: ","metadata"));
     }
 
     @Override
@@ -34,7 +41,9 @@ public class MetadataEventHandlerSupplier implements Supplier<List<TreeEventHand
 
         return asList(
                 new PDFEventHandler(properties, new JHoveCommandPipe(), resultCollector),
-                new NewsMLEventHandler(properties, documentCache, resultCollector)
+                //new NewsMLEventHandler(properties, documentCache, resultCollector),
+                new SchemaValidatorEventHandler(resultCollector,documentCache,attributeConfigs),
+                new SchematronValidatorEventHandler(resultCollector,documentCache,attributeConfigs)
         );
     }
 }
